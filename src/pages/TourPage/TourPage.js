a9167feaffb36abe1ec9863ccb2f7082f1bd3879
x3mart <x3mat@gmail.com>
2022-12-13 16:51:39 +0300

    TourPage.js fix

--- src/pages/TourPage/TourPage.js
+++ src/pages/TourPage/TourPage.js
@@ -167,7 +167,7 @@ const TourPage = ({
                   />
                   <div className={styles.divider}/>
                   {tour_preview.main_impressions && tour_preview.main_impressions.length > 0 && <TourImpressions impressions={tour_preview.main_impressions}/>}
-                  {((tour_preview.plan && tour_preview.plan.length > 0) || tour_preview.media_link) && <TourReview text={tour_preview.description} activities={tour_preview.plan} video={tour_preview && tour_preview.media_link ? tour_preview.media_link : ''}/>}
+                  {((tour_preview.plan && tour_preview.plan.length > 0) || tour_preview.media_link || tour_preview.description) && <TourReview text={tour_preview.description} activities={tour_preview.plan} video={tour_preview && tour_preview.media_link ? tour_preview.media_link : ''}/>}
                   {tour_preview.tour_images && tour_preview.tour_images.length > 0 && <TourGallery gallery={tour_preview.tour_images}/>}
                   {tour_preview.start_date && tour_preview.start_city && <TourRoute
                     ya_map={tour_preview && tour_preview.map}
